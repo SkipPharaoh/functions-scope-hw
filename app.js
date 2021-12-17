1012bff6f51c5b4c6afb24401502d7f29eaf4050
skippharaoh <caniggiathompson@gmail.com>
2021-12-16 20:08:49 -0500

    Commit 1 - Verbal questions.

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log('JS Linked')
+
+// 1) VERBAL QUESTIONS
+
+// Parameter - The variable(s) that are in the created function
+// Argument - The real data that is passed into the function
+// Return - This brings back a value from a function
+// Console.log - This is used for debugging and/or to print a value into the console
+// The implications of the return function is to bring back the final result of calculations and/or tasks
+
