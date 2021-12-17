3e90fa159a56420ce837662c4b7fcaf3132a8979
skippharaoh <caniggiathompson@gmail.com>
2021-12-16 20:45:50 -0500

    Commit 5 - Sum Array.

--- app.js
+++ app.js
@@ -29,3 +29,14 @@ function getTwoLengths(str1,str2){
     return [str1.length, str2.length]
 }
 console.log(getTwoLengths("Hank", "Hippopopalous"));
+
+// 5) SUM ARRAY
+
+function sumArray(arr){
+    let sum = 0
+    for (let i=0; i < arr[i]; i++){
+        sum += arr[i]
+    }
+    return sum
+}
+console.log(sumArray([1, 2, 3, 4, 5, 6]));
